b51aae04e8a9f65f81dbcc5b3017e566a1ebded9
iwamotokasumi <kpe.359@icloud.com>
2016-07-18 11:05:19 +0900

    Seconds Commit (リストの作成)

--- SwiftEditData/ListViewController.swift
+++ SwiftEditData/ListViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    var manager: NSFileManager!
+    var fullPath: String!
+    var paths: Array<String>!
+    @IBOutlet weak var myTableView: UITableView!
+    @IBAction func tapBack(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+        if paths == nil {
+            return 1
+        }
+        return paths.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        cell.textLabel?.text = paths[indexPath.row]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let storyboard = UIStoryboard(name:"Main",bundle:nil)
+        let controller: SelectViewController = storyboard.instantiateViewControllerWithIdentifier("SelectViewController") as! SelectViewController
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    func refreshPaths() {
+        paths = manager.subpathsAtPath(fullPath)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        refreshPaths()
+        self.myTableView.reloadData()
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        manager = NSFileManager.defaultManager()
+        fullPath = NSHomeDirectory() + "/Documents"
         // Do any additional setup after loading the view.
     }
 
